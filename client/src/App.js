7ea55da983f106a1da01956223d129e2bb05a3f8
justblack1227 <60590971+justblack1227@users.noreply.github.com>
2021-07-17 18:59:36 -0400

    add basename

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ class App extends PureComponent  {
 
   render() {
     return (
-      <BrowserRouter basename="/Project-10" >
+      <BrowserRouter basename="/Project-10/" >
       <div>
         <HeaderWithContext />
         <Switch>
