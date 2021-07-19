27364007b1d29e740066acd14b7beadafe76607a
Justin B <justblack1227@gmail.com>
2021-07-18 20:59:26 -0400

    changed config.js to interact with live host.

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,7 @@
 
 export default {
-  apiBaseUrl: 'http://localhost:5000/api',
+  apiBaseUrl: 'https://full-stack-course-app-api.herokuapp.com/',
+  
+  // For development purposes, if you want use the rest Api and react app on the same host. 
+  // apiBaseUrl: 'http://localhost:5000/api',
 };
